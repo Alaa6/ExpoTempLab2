dc1c77e81854247d5189b747a07d0e86f69d09ff
alaa6 <alaa.ashraf775@gmail.com>
2021-02-15 20:05:17 +0200

    ubdate

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,22 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import React , {useState} from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
-export default function Button({ btnName, bgColor, BtnBorderRadius ,txtColor  ,onPress , width , height , flexBasis}) {
+export default function Button({ btnName, bgColor, BtnBorderRadius, txtColor, onPress, width, height, flexBasis }) {
 
     return (
-        <TouchableOpacity onPress ={onPress}>
-            <View style={{ backgroundColor :bgColor ,
-                           width : width , 
-                           height : height , 
-                           borderRadius:BtnBorderRadius , 
-                        //    flex :1 , 
-                        // flexBasis : flexBasis ,
-                           justifyContent :'center' , 
-                           alignItems: 'center'}}>
+        <TouchableOpacity onPress={onPress}>
+            <View style={{
+                backgroundColor: bgColor,
+                width: width,
+                height: height,
+                borderRadius: BtnBorderRadius,
+                //    flex :1 , 
+                // flexBasis : flexBasis ,
+               
+                justifyContent: 'center',
+                alignItems: 'center'
+            }}>
 
-   
-            <Text style={{ color : txtColor , fontSize: 18,}}>{btnName}</Text>
+
+                <Text style={{ color: txtColor, fontSize: 18}}>{btnName}</Text>
             </View>
 
 
